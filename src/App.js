f08e912d0f6f6a7cb652265d7b41e26790b94bb2
unknown <kgy424@naver.com>
2023-06-07 17:25:30 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // const[상태값(state),상태값을 바꾸기 위한 함수]=초기값(useState(""))
+  const[username,setUsername]=useState("");
+  const[password,setPassword]=useState("");
+  const onSubmit=(eve)=>{
+    console.log(username,password)
+    eve.preventDefault();
+    // form은 전송 후에 refresh(새로고침)이 됨.
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <form onSubmit={onSubmit}>
+  <input placeholder='Username' value={username}
+  onChange={(eve)=>{setUsername(eve.target.value)}}
+  /><br/>
+  <input placeholder='Password' value={password}
+  onChange={(eve)=>{
+    setPassword(eve.target.value)
+    console.log(password)}}
+  /><br/>
+  <button type='submit'>Update</button>
+  </form>
+    </>
   );
 }
 
